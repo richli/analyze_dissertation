fe6230742624bbec43303335f12ab64e0155497f
Richard Lindsley <rich@dranek.com>
2016-07-03 20:37:19 -0700

    Use commas in output

--- dump_stats.sh
+++ dump_stats.sh
@@ -15,7 +15,7 @@ pushd "$repo"
 
 # Note the timestamp for each commit
 echo "Recording commit timestamps"
-git log --no-abbrev-commit --pretty=format:"%H %ai" > "$timestamps"
+git log --no-abbrev-commit --pretty=format:"%H,%aI" > "$timestamps"
 
 # Run texcount for each commit
 echo "Counting words for each commit"
@@ -29,7 +29,7 @@ for rev in "${revs[@]}"; do
     # The first few commits don't have a "manuscript" directory, so this checks
     # for the directory existence to prevent failure of the globbing
     if [[ -d "manuscript" ]]; then
-        echo -n "$rev " >> "$word_stats"
+        echo -n "$rev," >> "$word_stats"
         texcount -total -brief manuscript/*.tex >> "$word_stats"
     fi
 done
